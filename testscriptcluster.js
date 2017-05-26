eeec9108b874a83e846296e34324992c504156f5
Avinash Ponugupati <avinash.ponugupati@gmail.com>
2017-05-26 12:18:07 +0530

    base files

---
+++ testscriptcluster.js
@@ -0,0 +1,58 @@
+var map = L.map('image-map', {
+  
+  minZoom: 0,
+  maxZoom: 4,
+  
+  attributionControl: false
+}).setView([0,0], 1 );
+
+$.getJSON("testData.geojson",function(data){
+    var ratIcon = L.icon({
+      iconUrl: 'pin.png',
+      iconSize: [75,50]
+    });
+    var rodents = L.geoJson(data,{
+      pointToLayer: function(feature,latlng){
+        var marker = L.marker(latlng,{icon: ratIcon});
+        marker.bindPopup(feature.properties.Location);
+        return marker;
+      }
+    }).addTo(map);
+    var clusters = L.markerClusterGroup();
+    
+    map.addLayer(clusters);
+  });
+
+L.control.attribution({
+  prefix: false
+
+}).addAttribution('Floor Plan @ Demo').addTo(map);
+
+// dimensions of the image
+var w = 1280 * 2,
+    h = 806 * 2,
+    url = 'floor_plan.svg';
+
+// calculate the edges of the image, in coordinate space
+var southWest = map.unproject([0, h], map.getMaxZoom()-1);
+var northEast = map.unproject([w, 0], map.getMaxZoom()-1);
+var bounds = new L.LatLngBounds(southWest, northEast);
+
+// add the image overlay, 
+// so that it covers the entire map
+L.imageOverlay(url, bounds).addTo(map);
+
+// tell leaflet that the map is exactly as big as the image
+
+map.setMaxBounds(bounds);
+
+
+
+
+
+
+
+
+
+                    
+
